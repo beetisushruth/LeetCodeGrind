8abfa7c14d40b404a793b4b9dbce91c2719f9c05
Beeti Sushruth <sushruthsunny369@gmail.com>
2022-01-12 17:30:02 -0500

    Time: 3 ms (29.64%), Space: 50.6 MB (28.32%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.java
@@ -24,20 +24,11 @@ class Solution {
         }
         
         for(int i=1; i<matrix.length; i++) {
-            if(matrix[i][0] == 0) {
-                for(int j=0; j<matrix[0].length; j++) {
-                    matrix[i][j] = 0;
-                }
+            for(int j=1; j<matrix[0].length; j++) {
+                if(matrix[i][0] == 0 || matrix[0][j] == 0) matrix[i][j] = 0;
             }
         }
         
-        for(int i=1; i<matrix[0].length; i++) {
-            if(matrix[0][i] == 0) {
-                for(int j=0; j<matrix.length; j++) {
-                    matrix[j][i] = 0;
-                }
-            }
-        }
         if(firstRowZero) {
             for(int i=0; i<matrix[0].length; i++) {
                 matrix[0][i] = 0;
