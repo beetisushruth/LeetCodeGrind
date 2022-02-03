b29bad4ddaabdd8b601b414f9820a829dd916e43
Beeti Sushruth <sushruthsunny369@gmail.com>
2022-02-03 12:08:02 -0500

    Time: 477 ms (5.02%), Space: 93.7 MB (5.19%) - LeetHub

---
+++ 454-4sum-ii/454-4sum-ii.java
@@ -0,0 +1,55 @@
+class Solution {
+    public int fourSumCount(int[] nums1, int[] nums2, int[] nums3, int[] nums4) {
+        int a = nums1.length;
+        int b = nums2.length; 
+        int c = nums3.length; 
+        int d = nums4.length;
+        long[] nums12 = new long[a*b];
+        long[] nums34 = new long[c*d];
+        fillArray(nums1, nums2, nums12);
+        fillArray(nums3, nums4, nums34);
+        Arrays.sort(nums12);
+        Arrays.sort(nums34);
+        return findCount(nums12, nums34);
+    }
+    
+    public void fillArray(int[] nums1, int[] nums2, long[] nums) {
+        int k = 0;
+        for(int i=0; i<nums1.length; i++) {
+            for(int j=0; j<nums2.length; j++) {
+                nums[k] = nums1[i] + nums2[j];
+                k++;
+            }
+        }
+    }
+    
+    public int findCount(long[] a, long[] b) {
+        int count = 0;
+        Map<Long, Integer> mapA = new HashMap<>();
+        Map<Long, Integer> mapB = new HashMap<>();
+        for(long num : a) {
+            mapA.put(num, mapA.getOrDefault(num, 0) + 1);
+        }
+        for(long num : b) {
+            mapB.put(num, mapB.getOrDefault(num, 0) + 1);
+        }
+        for(long key : mapA.keySet()) {
+            if(mapB.containsKey(-key)) {
+                count += (mapA.get(key) * mapB.get(-key));
+            }
+        }
+        return count;
+    }
+    
+    public int findZeroes(long[] a, long[] b) {
+        int count = 0;
+        int count1 = 0, count2 = 0;
+        for(int i=0; i<a.length; i++) {
+            if(a[i] == 0) count1++;
+        }
+        for(int j=0; j<a.length; j++) {
+            if(b[j] == 0) count2++;
+        }
+        return count1*count2;
+    }
+}
