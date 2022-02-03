cdcea421f5b419be8701bef5cc909f533fe58978
Beeti Sushruth <sushruthsunny369@gmail.com>
2022-02-03 12:15:55 -0500

    Time: 182 ms (46.46%), Space: 62.7 MB (13.51%) - LeetHub

--- 454-4sum-ii/454-4sum-ii.java
+++ 454-4sum-ii/454-4sum-ii.java
@@ -1,14 +1,20 @@
 class Solution {
     public int fourSumCount(int[] nums1, int[] nums2, int[] nums3, int[] nums4) {
-        int a = nums1.length;
-        int b = nums2.length; 
-        int c = nums3.length; 
-        int d = nums4.length;
-        long[] nums12 = new long[a*b];
-        long[] nums34 = new long[c*d];
-        fillArray(nums1, nums2, nums12);
-        fillArray(nums3, nums4, nums34);
-        return findCount(nums12, nums34);
+        Map<Integer, Integer> map = new HashMap<>();
+        int count = 0;
+        for(int i=0; i<nums1.length; i++) {
+            for(int j=0; j<nums2.length; j++) {
+                map.put(nums1[i]+ nums2[j], map.getOrDefault(nums1[i]+nums2[j], 0) + 1);
+            }
+        }
+        for(int i=0; i<nums3.length; i++) {
+            for(int j=0; j<nums4.length; j++) {
+                if(map.containsKey(-(nums3[i]+nums4[j]))) {
+                    count += map.get(-(nums3[i]+nums4[j]));
+                }
+            }
+        }
+        return count;
     }
     
     public void fillArray(int[] nums1, int[] nums2, long[] nums) {
