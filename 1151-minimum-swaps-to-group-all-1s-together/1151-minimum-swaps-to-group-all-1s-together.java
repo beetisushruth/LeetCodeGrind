bf7cbde35a9f030e7a3708c3665f4475d748811a
Beeti Sushruth <sushruthsunny369@gmail.com>
2022-05-26 00:59:30 -0400

    Time: 7 ms (68.34%), Space: 74.5 MB (22.38%) - LeetHub

--- 1151-minimum-swaps-to-group-all-1s-together/1151-minimum-swaps-to-group-all-1s-together.java
+++ 1151-minimum-swaps-to-group-all-1s-together/1151-minimum-swaps-to-group-all-1s-together.java
@@ -7,13 +7,14 @@ class Solution {
             ones += data[i];
         }
         int swaps = 0;
-        int minSwaps = data.length;
-        for(int i=0; i<data.length; i++) {
+        for(int i=0; i<ones; i++) {
             if(data[i] == 0) swaps++;
-            if(i >= ones) {
-                if(data[i - ones] == 0) swaps--;
-            }
-            if(i >= ones - 1) minSwaps = Math.min(swaps, minSwaps);
+        }
+        int minSwaps = swaps;
+        for(int i=ones; i<data.length; i++) {
+            if(data[i] == 0) swaps++;
+            if(data[i - ones] == 0) swaps--;
+            minSwaps = Math.min(swaps, minSwaps);
         }
         return minSwaps;
     }
