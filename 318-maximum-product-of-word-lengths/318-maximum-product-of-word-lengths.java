aa2d941739f2e081576061694afb46b25a4e4259
Beeti Sushruth <sushruthsunny369@gmail.com>
2022-05-28 22:58:01 -0400

    Time: 54 ms (40.33%), Space: 47.9 MB (40.33%) - LeetHub

--- 318-maximum-product-of-word-lengths/318-maximum-product-of-word-lengths.java
+++ 318-maximum-product-of-word-lengths/318-maximum-product-of-word-lengths.java
@@ -1,29 +1,19 @@
 class Solution {
     public int maxProduct(String[] words) {
-        int[] count = new int[words.length];
-        List<Set<Character>> l = new ArrayList<>();
-        for(int i=0; i<words.length; i++) {
-            count[i] = words[i].length();
-            Set<Character> set = new HashSet<>();
-            for(int j=0; j<words[i].length(); j++) {
-                set.add(words[i].charAt(j));
+        int n = words.length;
+        Map<Integer, Integer> map = new HashMap<>();
+        for(String s : words) {
+            int mask = 0;
+            for(int i=0; i<s.length(); i++) {
+                mask |= 1 << (s.charAt(i) - 'a');
             }
-            l.add(set);
+            map.put(mask, Math.max(map.getOrDefault(mask, 0), s.length()));
         }
         int max = 0;
-        for(int i=0; i<words.length; i++) {
-            for(int j=i + 1; j<words.length; j++) {
-                boolean valid = true;
-                Set<Character> set1 = l.get(i);
-                Set<Character> set2 = l.get(j);
-                for(Character c : set1) {
-                    if(set2.contains(c))  {
-                        valid = false;
-                        break;
-                    }
-                }
-                if(valid) max = Math.max(max, count[i]*count[j]);
-            }
+        for(int mask1 : map.keySet()) {
+            for(int mask2 : map.keySet()) {
+                if((mask1 & mask2) == 0) max = Math.max(max, map.get(mask1) * map.get(mask2));
+            }  
         }
         return max;
     }
