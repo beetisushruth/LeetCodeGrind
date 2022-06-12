3021c551293577bb7fb34bcdd74aa0a443c780e7
Beeti Sushruth <sushruthsunny369@gmail.com>
2022-06-12 19:22:04 -0400

    Time: 12 ms (70.98%), Space: 44.6 MB (32.18%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.java
@@ -3,9 +3,8 @@ class Solution {
         List<Integer> sub = new ArrayList<>();
         sub.add(nums[0]);
         for(int i=1; i<nums.length; i++) {
-            if(sub.get(sub.size() - 1) < nums[i]) {
-                sub.add(nums[i]);
-            } else {
+            if(sub.get(sub.size() - 1) < nums[i]) sub.add(nums[i]);
+            else {
                 int index = binarySearch(sub, nums[i]);
                 sub.set(index, nums[i]);
             }
@@ -13,14 +12,14 @@ class Solution {
         return sub.size();
     }
     
-    public int binarySearch(List<Integer> sub, int num) {
-        int size = sub.size();
+    public int binarySearch(List<Integer> list, int num) {
         int left = 0;
-        int right = size - 1;
-        int mid = (left + right)/2;
+        int right = list.size() - 1;
+        int mid;
         while(left < right) {
             mid = (left + right)/2;
-            if(sub.get(mid) >= num) {
+            if(list.get(mid) == num) return mid;
+            else if(list.get(mid) > num) {
                 right = mid;
             } else {
                 left = mid + 1;
