dd4c5d886ff9b910cfd5d45c4490c6bfae4b085e
Beeti Sushruth <sushruthsunny369@gmail.com>
2022-06-22 22:36:53 -0400

    Time: 0 ms (100.00%), Space: 41.1 MB (33.98%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.java
+++ 70-climbing-stairs/70-climbing-stairs.java
@@ -1,10 +1,12 @@
 class Solution {
     public int climbStairs(int n) {
-        int[] dp = new int[n + 1];
-        dp[0] = 1; dp[1] = 1;
+        if(n < 2) return 1;
+        int left = 1; int right = 1;
         for(int i=2; i<=n; i++) {
-            dp[i] = dp[i - 1] + dp[i - 2];
+            int temp = right;
+            right = left + right;
+            left = temp;
         }
-        return dp[n];
+        return right;
     }
 }
