ae41d28513270e6c9570c43e611e11c46160944d
Beeti Sushruth <sushruthsunny369@gmail.com>
2022-06-28 21:30:06 -0400

    Time: 30 ms (32.03%), Space: 54.9 MB (32.42%) - LeetHub

--- 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.java
+++ 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.java
@@ -1,27 +1,33 @@
 class Solution {
     public int minDeletions(String s) {
-        int n = s.length();
-        int[] arr = new int[26];
-        SortedSet<Integer> sset = new TreeSet<>();
-        Set<Integer> set = new HashSet<>();
-        for(int i=0; i<n; i++) {
-            arr[s.charAt(i) - 'a']++;
+        // Store the frequency of each character
+        int[] frequency = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            frequency[s.charAt(i) - 'a']++;
         }
-        for(int i=0; i<26; i++) {
-            if(arr[i] > 0) sset.add(arr[i]);
-        }
-        int count = 0;
-        for(int i=0; i<26; i++) {
-            if(set.contains(arr[i]) && arr[i] > 0) {
-                int freq = arr[i];
-                while(sset.contains(freq)) {
-                    freq--;
-                }
-                count += arr[i] - freq;
-                if(freq > 0) sset.add(freq);
+        
+        // Add the frequencies to the priority queue
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        for (int i = 0; i < 26; i++) {
+            if (frequency[i] > 0) {
+                pq.add(frequency[i]);
             }
-            set.add(arr[i]);
         }
-        return count;
+        
+        int deleteCount = 0;
+        while (pq.size() > 1) {
+            int topElement  = pq.remove();
+            
+            // If the top two elements in the priority queue are the same
+            if (topElement == pq.peek()) {
+                // Decrement the popped value and push it back into the queue
+                if (topElement - 1 > 0) {
+                    pq.add(topElement - 1);
+                }
+                deleteCount++;
+            }
+        }
+        
+        return deleteCount;
     }
 }
