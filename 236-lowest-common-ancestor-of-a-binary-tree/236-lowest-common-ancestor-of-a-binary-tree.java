d7cc2e182dccb3c5742f4ed4183ed5ee6706c347
Beeti Sushruth <sushruthsunny369@gmail.com>
2022-07-26 01:27:31 -0400

    Time: 10 ms (58.82%), Space: 47.6 MB (33.80%) - LeetHub

--- 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.java
+++ 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.java
@@ -1,47 +1,41 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode(int x) { val = x; }
- * }
- */
 class Solution {
-    List<TreeNode> pList;
-    List<TreeNode> qList;
+
+    private TreeNode ans;
+
+    public Solution() {
+        // Variable to store LCA node.
+        this.ans = null;
+    }
+
+    private boolean recurseTree(TreeNode currentNode, TreeNode p, TreeNode q) {
+
+        // If reached the end of a branch, return false.
+        if (currentNode == null) {
+            return false;
+        }
+
+        // Left Recursion. If left recursion returns true, set left = 1 else 0
+        int left = this.recurseTree(currentNode.left, p, q) ? 1 : 0;
+
+        // Right Recursion
+        int right = this.recurseTree(currentNode.right, p, q) ? 1 : 0;
+
+        // If the current node is one of p or q
+        int mid = (currentNode == p || currentNode == q) ? 1 : 0;
+
+
+        // If any two of the flags left, right or mid become True
+        if (mid + left + right >= 2) {
+            this.ans = currentNode;
+        }
+
+        // Return true if any one of the three bool values is True.
+        return (mid + left + right > 0);
+    }
+
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        getLineage(root, p, q, new ArrayList<>());
-        TreeNode ans = null;
-        int i = 0; int j = 0;
-        while(i < pList.size() && j < qList.size()) {
-            if(pList.get(i) == qList.get(j)) ans = pList.get(i);
-            else break;
-            i++; j++;
-        }
-        return ans;
-    }
-    
-    public void getLineage(TreeNode node, TreeNode p, TreeNode q, List<TreeNode> list) {
-        if(node == null) return;
-        list.add(node);
-        if(node == p) {
-            pList = clone(list); 
-        }
-        if(node == q) {
-            qList = clone(list);
-        }
-       // System.out.println(list.size() + " "+ node.val);
-        getLineage(node.left, p, q, list);
-        getLineage(node.right, p, q, list);
-        list.remove(list.size() - 1);
-    }
-    
-    public List<TreeNode> clone(List<TreeNode> arr) {
-        List<TreeNode> ans = new ArrayList<>();
-        for(int i=0; i<arr.size(); i++) {
-            ans.add(arr.get(i));
-        }
-        return ans;
+        // Traverse the tree
+        this.recurseTree(root, p, q);
+        return this.ans;
     }
 }
