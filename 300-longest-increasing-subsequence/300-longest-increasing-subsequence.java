ca864d1bb5bba217c0da4c32baf291172e167308
Beeti Sushruth <sushruthsunny369@gmail.com>
2022-08-08 17:07:48 -0400

    Time: 11 ms (76.23%), Space: 44.4 MB (55.89%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.java
@@ -1,30 +1,32 @@
 class Solution {
     public int lengthOfLIS(int[] nums) {
-        List<Integer> sub = new ArrayList<>();
-        sub.add(nums[0]);
-        for(int i=1; i<nums.length; i++) {
-            if(sub.get(sub.size() - 1) < nums[i]) sub.add(nums[i]);
-            else {
-                int index = binarySearch(sub, nums[i]);
-                sub.set(index, nums[i]);
+        List<Integer> arr = new ArrayList<>();
+        int max = 0;
+        for(int num : nums) {
+            if(arr.size() == 0 || arr.get(arr.size() - 1) < num) {
+                arr.add(num);
+            } else {
+                int index = binarySearch(arr, num);
+                arr.set(index, num);
             }
+            max = Math.max(max, arr.size());
         }
-        return sub.size();
+        return max;
     }
     
-    public int binarySearch(List<Integer> list, int num) {
-        int left = 0;
-        int right = list.size() - 1;
-        int mid;
-        while(left < right) {
-            mid = (left + right)/2;
-            if(list.get(mid) == num) return mid;
-            else if(list.get(mid) > num) {
-                right = mid;
+    public int binarySearch(List<Integer> nums, int num) {
+        int start = 0;
+        int end = nums.size() - 1;
+        int ans = -1;
+        while(start <= end) {
+            int mid = start + (end - start)/2;
+            if(nums.get(mid) >= num) {
+                ans = mid;
+                end = mid - 1;
             } else {
-                left = mid + 1;
+                start = mid + 1;
             }
         }
-        return left;
+        return ans;
     }
 }
