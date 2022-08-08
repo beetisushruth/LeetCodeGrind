ed0996e838ee53505826736a1cad531ee08301f4
Beeti Sushruth <sushruthsunny369@gmail.com>
2022-08-08 17:18:10 -0400

    Time: 3 ms (84.77%), Space: 42.8 MB (86.22%) - LeetHub

--- 695-max-area-of-island/695-max-area-of-island.java
+++ 695-max-area-of-island/695-max-area-of-island.java
@@ -1,25 +1,28 @@
 class Solution {
+    
+    public static int[][] dirs = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
     public int maxAreaOfIsland(int[][] grid) {
         int m = grid.length;
         int n = grid[0].length;
-        int maxArea = 0;
         boolean[][] visited = new boolean[m][n];
+        int maxArea = 0;
         for(int i=0; i<m; i++) {
             for(int j=0; j<n; j++) {
-                maxArea = Math.max(dfs(grid, i, j, visited), maxArea);
+                int area = getArea(grid, visited, i, j);
+                maxArea = Math.max(area, maxArea);
             }
         }
         return maxArea;
     }
     
-    public int dfs(int[][] grid, int i, int j, boolean[][] visited) {
+    public int getArea(int[][] grid, boolean[][] visited, int i, int j) {
+        if(i < 0 || j < 0 || i >= grid.length || j >= grid[0].length) return 0;
         if(grid[i][j] == 0 || visited[i][j]) return 0;
         visited[i][j] = true;
-        int sum = 1;
-        if(i + 1 < grid.length && !visited[i + 1][j]) sum += dfs(grid, i + 1, j, visited);
-        if(i - 1 >= 0 && !visited[i - 1][j]) sum += dfs(grid, i - 1, j, visited);
-        if(j + 1 < grid[0].length && !visited[i][j + 1]) sum += dfs(grid, i, j + 1, visited);
-        if(j - 1 >= 0 && !visited[i][j - 1]) sum += dfs(grid, i, j - 1, visited);
-        return sum;
+        int count = 1;
+        for(int[] dir : dirs) {
+            count += getArea(grid, visited, i + dir[0], j + dir[1]);
+        }
+        return count;
     }
 }
