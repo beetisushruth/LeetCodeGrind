3ac1ef5c2408b26fbe9ee529d8fc8761817f7347
Beeti Sushruth <sushruthsunny369@gmail.com>
2022-08-23 16:42:41 -0400

    Time: 339 ms (5.55%), Space: 118.7 MB (8.03%) - LeetHub

--- 472-concatenated-words/472-concatenated-words.java
+++ 472-concatenated-words/472-concatenated-words.java
@@ -4,17 +4,18 @@ class Solution {
         for(String word: words) set.add(word);
         List<String> ans = new ArrayList<>();
         for(String word : words) {
-            if(isCorrectWord(word, 0, 0, set)) ans.add(word);
+            if(isCorrectWord(word, 0, 0, set, new HashMap<>())) ans.add(word);
         }
         return ans;
     }
     
-    public boolean isCorrectWord(String word, int start, int end, Set<String> set) {
+    public boolean isCorrectWord(String word, int start, int end, Set<String> set, Map<String, Boolean> dp) {
         //System.out.println(start+" "+end);
         if(end == start && end == word.length()) return true;
         if(end > word.length()) return false;
-        boolean res = set.contains(word.substring(start, end)) && end - start != word.length() && isCorrectWord(word, end, end, set);
-        res = res | isCorrectWord(word, start, end + 1, set);
+        boolean res = set.contains(word.substring(start, end)) && end - start != word.length() && (dp.containsKey(start+" "+end) ? dp.get(start+" "+end) : isCorrectWord(word, end, end, set, dp));
+        res = res | (dp.containsKey(start+" "+(end + 1)) ? dp.get(start+" "+(end + 1)) : isCorrectWord(word, start, end + 1, set, dp));
+        dp.put(start+" "+end, res);
         return res;
     }
 }
