1480fdee3236bbdc29331a4e4deb532325dbd6d9
Beeti Sushruth <sushruthsunny369@gmail.com>
2022-08-23 23:32:17 -0400

    Time: 35 ms (32.10%), Space: 42.3 MB (90.11%) - LeetHub

--- 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.java
+++ 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.java
@@ -1,16 +1,32 @@
 class Solution {
     public long subArrayRanges(int[] nums) {
         int n = nums.length;
-        long count = 0;
+        return getTotal(nums, n, true) - getTotal(nums, n, false);
+    }
+    
+    public long getTotal(int[] nums, int n, boolean isMaximum) {
+        long total = 0;
+        int[] left = new int[n];
+        int[] right = new int[n];
+        Stack<Integer> st = new Stack<>();
         for(int i=0; i<n; i++) {
-            int min = nums[i];
-            int max = nums[i];
-            for(int j = i + 1; j<n; j++) {
-                min = Math.min(min, nums[j]);
-                max = Math.max(max, nums[j]);
-                count += (max - min);
+            while(!st.isEmpty() && (isMaximum ? nums[st.peek()] <= nums[i] : nums[st.peek()] >= nums[i])) {
+                st.pop();
             }
+            left[i] = st.isEmpty() ? -1 : st.peek(); 
+            st.add(i);
         }
-        return count;
+        st = new Stack<>();
+        for(int i=n-1; i>=0; i--) {
+            while(!st.isEmpty() && (isMaximum ? nums[st.peek()] < nums[i] : nums[st.peek()] > nums[i])) {
+                st.pop();
+            }
+            right[i] = st.isEmpty() ? n : st.peek(); 
+            st.add(i);
+        }
+        for(int i=0; i<n; i++) {
+            total += (long) (i - left[i])*(right[i] - i)*nums[i];
+        }
+        return total;
     }
 }
