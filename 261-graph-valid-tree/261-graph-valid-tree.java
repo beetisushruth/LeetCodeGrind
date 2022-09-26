1282e5fbfe8df21dc0faa20da24a8b8f2d3d0927
Beeti Sushruth <sushruthsunny369@gmail.com>
2022-09-26 00:22:07 -0400

    Time: 2 ms (69.30%), Space: 47 MB (41.47%) - LeetHub

---
+++ 261-graph-valid-tree/261-graph-valid-tree.java
@@ -0,0 +1,41 @@
+class Solution {
+    int[] parent;
+    int[] rank;
+    
+    public boolean validTree(int n, int[][] edges) { 
+        parent = new int[n];
+        rank = new int[n];
+        for(int i=0; i<n; i++) parent[i] = i;
+        for(int i=0; i<edges.length; i++) {
+            int[] edge = edges[i];
+            // System.out.println(edge[0]+" "+parent[edge[0]]+" "+edge[1]+" "+parent[edge[1]]);
+            if(find(edge[0]) == find(edge[1])) return false;
+            union(edge[0], edge[1]);
+        }
+        int rootParent = find(0);
+        for(int i=1; i<n; i++) {
+            if(find(i) != rootParent) return false;
+        }
+        return true;
+    }
+    
+    public void union(int x, int y) {
+        int u = find(x);
+        int v = find(y);
+        if(rank[u] < rank[v]) {
+            parent[u] = v;
+        } else if(rank[u] > rank[v]) {
+            parent[v] = u;
+        } else {
+            parent[v] = u;
+            rank[u]++;
+        }
+    }
+    
+    public int find(int x) {
+        if(parent[x] != x) {
+            return parent[x] = find(parent[x]);
+        }
+        return parent[x];
+    }
+}
