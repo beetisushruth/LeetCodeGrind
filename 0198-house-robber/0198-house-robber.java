26664a3f176725e8c36a4a4fb1711979e04c6ddc
Beeti Sushruth <sushruthsunny369@gmail.com>
2024-01-21 00:09:58 -0500

    Time: 0 ms (100.00%), Space: 40.7 MB (49.20%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,9 +1,8 @@
 class Solution {
     public int rob(int[] nums) {
         int n = nums.length;
-        if(n == 1) return nums[0];
-        int prev1 = nums[0]; int prev2 = Math.max(nums[1], nums[0]);
-        for(int i=2; i<n; i++) {
+        int prev1 = 0; int prev2 = 0;
+        for(int i=0; i<n; i++) {
             int tmp = prev2;
             prev2 = Math.max(prev1 + nums[i], prev2);
             prev1 = tmp;
